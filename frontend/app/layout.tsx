03b138c8b7fb25cae02a596bf04cd7d42f7c9a0c
Suzit-Datta <suzitdatta10@gmail.com>
2026-09-24 12:08:57 +0600

    daisyui bug fixed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,7 +31,9 @@ export default function RootLayout({
       data-theme="reviewness"
       className={`${spaceGrotesk.variable} ${inter.variable}`}
     >
-      <body>{children}</body>
+      <body suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
